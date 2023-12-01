c08ca04b51bb0fe4071623ccee6fe6ff146044d3
Timur <raxmatovtimurjava@gmail.com>
2023-12-01 18:19:19 +0500

    Private layout has been created

--- src/Layout/Private/Private.tsx
+++ src/Layout/Private/Private.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet , Navigate } from "react-router-dom";
 import Header from "../../Components/Header";
-
+import { KEY } from "../../constains/HeaderCase";
 const Private = () => {
-  return (
+  return KEY ? (
     <>
       <Header />
       <Outlet />
     </>
-  );
+  ) : <Navigate to="/login"/>;
 };
 
 export default Private;
