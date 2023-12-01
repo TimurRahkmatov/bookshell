a12805425db69673649286bf5938c81d66f3b927
Timur <raxmatovtimurjava@gmail.com>
2023-12-01 18:17:32 +0500

    Public layout has been built

--- src/Layout/Public/Public.tsx
+++ src/Layout/Public/Public.tsx
@@ -1,8 +1,22 @@
-
+import { Outlet , Navigate} from "react-router-dom";
+import { Box } from "@mui/material";
+import BackgroundImage from "../../Images/background.png"
+import { KEY } from "../../constains/HeaderCase";
 const Public = () => {
-  return (
-    <div>Public</div>
-  )
-}
 
-export default Public
+  return KEY == null ? (
+    <>
+      <img
+        className="background_image"
+        src={BackgroundImage}
+        alt="background image"
+      />
+      <Box sx={{height: "100vh", display:"flex" , justifyContent: "center" , alignItems: "center" , }}>
+            <Outlet />
+        </Box>
+      <Outlet />
+    </>
+  ) : <Navigate to='/' />;
+};
+
+export default Public;
---
+++ src/constains/HeaderCase.tsx
@@ -0,0 +1,2 @@
+export const KEY = localStorage.getItem("Key");
+export const SECRET_KEY = localStorage.getItem("SecretKey");
