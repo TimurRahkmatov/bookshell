0c9242e808d11dc180889a6c3374d9da9878172b
Timur <raxmatovtimurjava@gmail.com>
2023-12-01 17:58:04 +0500

    The setup part of the project is built

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { Routes , Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Private from "./Layout/Private/Private";
+import Login from "./Pages/Login";
+import Public from "./Layout/Public";
+import Register from "./Pages/Register";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -9,6 +13,11 @@ function App() {
         <Route path="/" element={<Private />}>
             <Route path="/" element={<Home />}/>
         </Route>
+        <Route path="/" element={<Public />}>
+          <Route path="/login" element={<Login />}/>
+          <Route  path="/register" element={<Register />}/>
+        </Route>
+        <Route path="*" element={<NotFound />}/>
       </Routes>
     </>
   );
