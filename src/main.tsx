9c1adab65b6de86ff808b3a28d41cc9416a2e08c
Timur <raxmatovtimurjava@gmail.com>
2023-12-01 18:03:47 +0500

    Moved things to main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import axios from "axios";
+
+// Toastify
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+
+// Styles
 import "./index.css";
 
-// BrowserRouter
-import { BrowserRouter } from "react-router-dom";
+// Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+// axios
+axios.defaults.headers.common["Content-Type"] = "application/json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+      <ToastContainer theme="colored" />
+    </Router>
   </React.StrictMode>
 );
