e99009c28a043117e6e87a55ecc908bb3e0d5d9c
Timur <raxmatovtimurjava@gmail.com>
2023-12-02 04:28:00 +0500

    added 404 not found page design

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,7 +1,33 @@
+import BackgroundImage from "../../Images/background.png";
+import NotFoundImg from "../../Images/404.png"
+import { Box } from "@mui/material";
+import SubmitButton from "../../Components/Buttons/SubmitBtn";
+import { Link } from "react-router-dom";
+
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <Box>
+      <img
+        className="background_image"
+        src={BackgroundImage}
+        alt="background image"
+      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        <img src={NotFoundImg} alt="404 Page not found" />
+        <Box sx={{width: '400px' , display: "flex" , justifyContent: "space-between" , alignItems: "center" , gap: "2rem" , marginTop: '1rem'}} component="div"> 
+          <Link style={{width: "100%"}} to="/"><SubmitButton>Go Home page</SubmitButton></Link>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
