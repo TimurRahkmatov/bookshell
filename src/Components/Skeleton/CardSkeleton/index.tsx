162a478d409f4aca7bdfa6bf2cd578645282d7b7
Timur <raxmatovtimurjava@gmail.com>
2023-12-03 17:22:29 +0500

    edit skeleton

--- src/Components/Skeleton/CardSkeleton/index.tsx
+++ src/Components/Skeleton/CardSkeleton/index.tsx
@@ -3,16 +3,13 @@ import { Skeleton } from "@mui/material";
 const CardSkeleton = () => {
   return (
     <>
-      {[0, 1, 2].map((index: number) => (
+      {[0, 1].map((index: number) => (
         <Skeleton
           key={index}
           animation="wave"
           variant="rectangular"
-          sx={{
-            borderRadius: "7px",
-            backgroundColor: "#6d6d6d4e",
-            width: { lg: "370px", md: "380px", sm: "360px", xs: "300px" },
-          }}
+          height={200}
+          sx={{ borderRadius: "7px", backgroundColor: "#6d6d6d4e" , width: { lg: 370, md: 380, sm: 360, xs: 300 } }}
         />
       ))}
     </>
